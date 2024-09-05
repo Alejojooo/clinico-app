d225b5002484d6e568006594e0e01be585e74432
Alejojooo <alejandrofn2204@gmail.com>
2024-09-05 09:59:01 -0600

    feat: seleccion de modulo

--- src/renderer/src/components/ModuleButton.jsx
+++ src/renderer/src/components/ModuleButton.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types'
 
-export default function NavigationButton({ name, icon }) {
+export default function NavigationButton({ name, icon, isActive = false, onClick }) {
   return (
-    <button className="flex h-14 flex-row items-center justify-start gap-3 rounded-full py-4 pl-4 pr-6 transition-colors hover:bg-secondary-light">
+    <button
+      className={`flex h-14 flex-row items-center justify-start gap-3 rounded-full py-4 pl-4 pr-6 transition-colors hover:bg-secondary-light focus:bg-tertiary ${isActive ? 'bg-tertiary' : ''}`}
+      onClick={onClick}
+    >
       {icon}
       <h3 className="text-sm font-semibold text-accent">{name}</h3>
     </button>
@@ -11,5 +14,7 @@ export default function NavigationButton({ name, icon }) {
 
 NavigationButton.propTypes = {
   name: PropTypes.string.isRequired,
-  icon: PropTypes.element.isRequired
+  icon: PropTypes.element.isRequired,
+  isActive: PropTypes.bool,
+  onClick: PropTypes.func
 }
--- src/renderer/src/components/ModulesLayout.jsx
+++ src/renderer/src/components/ModulesLayout.jsx
@@ -1,19 +1,41 @@
 import ModuleButton from './ModuleButton'
-
 import {
   UserGroupIcon,
   ArchiveBoxIcon,
   BookOpenIcon,
   Cog6ToothIcon
 } from '@heroicons/react/24/outline'
+import { useState } from 'react'
 
 export default function ModulesLayout() {
+  const [activeIndex, setActiveIndex] = useState(0)
+
   return (
     <nav className="flex h-fit w-full flex-col">
-      <ModuleButton name="Pacientes" icon={<UserGroupIcon className="size-6" />}></ModuleButton>
-      <ModuleButton name="Fármacos" icon={<ArchiveBoxIcon className="size-6" />}></ModuleButton>
-      <ModuleButton name="Agenda" icon={<BookOpenIcon className="size-6" />}></ModuleButton>
-      <ModuleButton name="Herramientas" icon={<Cog6ToothIcon className="size-6" />}></ModuleButton>
+      <ModuleButton
+        name="Pacientes"
+        icon={<UserGroupIcon className="size-6" />}
+        isActive={activeIndex === 0}
+        onClick={() => setActiveIndex(0)}
+      ></ModuleButton>
+      <ModuleButton
+        name="Fármacos"
+        icon={<ArchiveBoxIcon className="size-6" />}
+        isActive={activeIndex === 1}
+        onClick={() => setActiveIndex(1)}
+      ></ModuleButton>
+      <ModuleButton
+        name="Agenda"
+        icon={<BookOpenIcon className="size-6" />}
+        isActive={activeIndex === 2}
+        onClick={() => setActiveIndex(2)}
+      ></ModuleButton>
+      <ModuleButton
+        name="Herramientas"
+        icon={<Cog6ToothIcon className="size-6" />}
+        isActive={activeIndex === 3}
+        onClick={() => setActiveIndex(3)}
+      ></ModuleButton>
     </nav>
   )
 }
