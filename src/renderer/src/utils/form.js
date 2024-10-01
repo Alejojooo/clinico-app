7a36156d802cac15ac4a12d127447393fc48a35a
Alejojooo <alejandrofn2204@gmail.com>
2024-10-01 15:39:08 -0600

    fix: limpieza de datos en el frontend

--- src/renderer/src/utils/form.js
+++ src/renderer/src/utils/form.js
@@ -2,7 +2,7 @@ export function clean(formData) {
   const newFormData = trim(formData)
   for (const field in formData) {
     const value = newFormData[field]
-    if (typeof value !== Boolean && !field) delete newFormData[field]
+    if (!value && typeof value !== Boolean) delete newFormData[field]
   }
   return newFormData
 }
